518836b57a270e8ca2dacd4bc23f16ae837ede23
Liafell <leneli.lehmann@yahoo.de>
2017-04-23 22:35:29 +0300

    Портфолио, основная верстка страниц

---
+++ portfolio/assets/js/app.js
@@ -0,0 +1,8 @@
+(function() {
+	"use strict";
+
+
+	svg4everybody(); //for SVG-elements in IE
+	console.log("hello APP!");
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oKSB7XHJcblx0XCJ1c2Ugc3RyaWN0XCI7XHJcblxyXG5cclxuXHRzdmc0ZXZlcnlib2R5KCk7IC8vZm9yIFNWRy1lbGVtZW50cyBpbiBJRVxyXG5cdGNvbnNvbGUubG9nKFwiaGVsbG8gQVBQIVwiKTtcclxufSkoKTsiXX0=
